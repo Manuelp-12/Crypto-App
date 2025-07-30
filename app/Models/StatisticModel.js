96ba08ea4b13c306d53bde790d3033a2b4ca7682
Manuel Paul <139383972+Manuelp-12@users.noreply.github.com>
2025-07-30 13:10:42 -0400

    Delete app directory

--- app/Models/StatisticModel.js
+++
@@ -1,9 +0,0 @@
-class StatisticModel {
-    constructor(data) {
-        this.title = data.title;
-        this.value = data.value;
-        this.percentageChange = data.percentageChange;
-    }
-}
-
-export default StatisticModel;

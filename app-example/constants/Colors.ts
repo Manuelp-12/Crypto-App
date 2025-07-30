c008c1fbd9c907643b3999130372a49f005257a5
Manuel Paul <139383972+Manuelp-12@users.noreply.github.com>
2025-07-30 13:53:43 -0400

    Add files via upload

---
+++ app-example/constants/Colors.ts
@@ -0,0 +1,35 @@
+/**
+ * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
+ * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
+ */
+
+const light_colors = {
+  primary: '#0496ff',
+  secondary: '#D3D3D3',
+  green_color: '#20bf55',
+  red_color: '#d81159',
+  accent: "#000000",
+  background_color: '#FFFFFF',
+  button_color: '#FFFFFF',
+  secondaryText: '#808080',
+  strong: '#FFFFFF',
+  modalbg: '#E3DAC9',
+};
+
+const dark_colors = {
+  primary: '#00a8e8',
+  secondary: '#00171f',
+  green_color: '#7ae582',
+  red_color: '#d00000',
+  accent: "#00a8e8",
+  background_color: '#121212',
+  button_color: '#2C2D2D',
+  secondaryText: '#808080',
+  strong: '#000000',
+  modalbg: '#00171f',
+}
+
+export default {
+  light_colors,
+  dark_colors,
+};
